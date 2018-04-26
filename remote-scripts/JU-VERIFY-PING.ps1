f501fe3bfb53c33f3d2a5e81c3b6d3687a1f9a9d
Shital Savekar <iamshital@outlook.com>
2018-04-26 16:38:30 -0700

    Adding a sample test

---
+++ remote-scripts/JU-VERIFY-PING.ps1
@@ -0,0 +1,57 @@
+﻿Import-Module .\TestLibs\RDFELibs.psm1 -Force
+$result = ""
+$testResult = ""
+$resultArr = @()
+
+$isDeployed = DeployVMS -setupType $currentTestData.setupType -Distro $Distro -xmlConfig $xmlConfig
+
+if ($isDeployed)
+{
+	try
+	{
+        LogMsg "Checking ping"
+        $vm1 = $allVMData[0]
+        $vm2 = $allVMData[1]
+        $out = RunLinuxCmd -username $user -password $password -ip $vm1.PublicIP -command "ping -c 10 $($vm2.InternalIP)"-port $vm1.SSHPort
+        LogMsg $out
+        if ($out -ne $null)
+        {
+            LogMsg "Test Result : PASS."
+            $testResult = "PASS"
+        }
+        else
+        {
+            LogMsg "Test Result : FAIL."
+            $testResult = "FAIL"
+		}
+	}
+	catch
+	{
+		$ErrorMessage =  $_.Exception.Message
+		LogMsg "EXCEPTION : $ErrorMessage"
+	}
+	Finally
+	{
+		$metaData = ""
+		if (!$testResult)
+		{
+			$testResult = "Aborted"
+		}
+		$resultArr += $testResult
+#$resultSummary +=  CreateResultSummary -testResult $testResult -metaData $metaData -checkValues "PASS,FAIL,ABORTED" -testName $currentTestData.testName# if you want to publish all result then give here all test status possibilites. if you want just failed results, then give here just "FAIL". You can use any combination of PASS FAIL ABORTED and corresponding test results will be published!
+	}
+}
+
+else
+{
+	$testResult = "FAIL"
+	$resultArr += $testResult
+}
+
+$result = GetFinalResultHeader -resultarr $resultArr
+
+#Clean up the setup
+DoTestCleanUp -result $result -testName $currentTestData.testName -deployedServices $isDeployed -ResourceGroups $isDeployed
+
+#Return the result and summery to the test suite script..
+return $result
